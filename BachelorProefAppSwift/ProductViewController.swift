a4503251ab99537222b5dd8d3b43652d827b88b5
Toon De True <toondetrue@toons-imac.local>
2017-08-22 15:40:19 +0200

    verder opbouw van product view

--- BachelorProefAppSwift/ProductViewController.swift
+++ BachelorProefAppSwift/ProductViewController.swift
@@ -12,6 +12,8 @@ import UIKit
 class ProductViewController:UIViewController{
     
     @IBOutlet weak var titlelbl: UILabel!
+    @IBOutlet weak var artistLbl: UILabel!
+    @IBOutlet weak var imageAlbum: UIImageView!
     
     var album:Album?;
     
@@ -27,5 +29,7 @@ class ProductViewController:UIViewController{
     
     private func fillPage(){
         titlelbl.text = album?.title;
+        artistLbl.text = "Made by " + (album?.artist)!;
+        imageAlbum.sd_setImage(with: URL(string: (album?.url)!), placeholderImage: UIImage(named:"placeholder.png"))
     }
 }
